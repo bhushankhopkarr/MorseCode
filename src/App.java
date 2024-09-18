8a3befaedf14c1ea9f95cc548005962691b3ea97
bhushankhopkarr <bhushankhopkar06@gmail.com>
2024-09-18 11:46:04 +0530

    Refactor GUI initialization for thread safety and exception handling

--- src/App.java
+++ src/App.java
@@ -1,14 +1,18 @@
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        // invokeLater insures that the GUI is created and updated on the Event Dispatch Thread
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new MorseCodeTranslatorGUI().setVisible(true);;
-            }
-        });
+        // Exception handling for potential issues during GUI initialization
+        try {
+            // Ensure the GUI is created and updated in a thread-safe manner
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new MorseCodeTranslatorGUI().setVisible(true);
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("An error occurred while launching the application: " + e.getMessage());
+        }
     }
-
 }
